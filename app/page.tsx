d94ba2f608769f7b7bf45794940ec381f71645a5
Michael P <mikepetschek@gmail.com>
2026-01-18 15:29:56 -0500

    fix light mode

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
   console.log(movies);
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start text-white">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start text-white dark:text-black">
         <div>
           {movies.map((movie) => (
             <div key={movie.name}>
